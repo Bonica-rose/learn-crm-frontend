fe20b9daf69d9cf242338d9aa8c77ad156467af7
Bonica-rose <eveningstarforever@gmail.com>
2026-07-14 21:27:55 +0530

    initial commit of project

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+            </Routes>
+        </BrowserRouter>
+    );
+}
